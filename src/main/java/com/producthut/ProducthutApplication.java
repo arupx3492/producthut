3020dab0ce9ae7074f2cfebe7c7c2f380b3d7722
Arup Mahato <arup71062@gmail.com>
2022-12-22 02:04:52 +0530

    1st

--- src/main/java/com/producthut/ProducthutApplication.java
+++ src/main/java/com/producthut/ProducthutApplication.java
@@ -1,4 +1,4 @@
-package com.producthut.producthut;
+package com.producthut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/producthut/ProducthutApplicationTests.java
+++ src/test/java/com/producthut/ProducthutApplicationTests.java
@@ -1,4 +1,4 @@
-package com.producthut.producthut;
+package com.producthut;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
