02004959d3331b25ce29e52c8a3c64c63350368a
Arup Mahato <arup71062@gmail.com>
2022-12-22 01:39:26 +0530

    Initial commit

---
+++ src/main/java/com/producthut/producthut/ProducthutApplication.java
@@ -0,0 +1,13 @@
+package com.producthut.producthut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducthutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducthutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/producthut/producthut/ProducthutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.producthut.producthut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducthutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
